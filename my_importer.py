7d76c6ef91e5a5eba54b1b88f7512b56cf694a51
y.bismuth <y.bismuth@criteo.com>
2017-05-15 17:45:12 +0200

    update

--- my_importer.py
+++ my_importer.py
@@ -10,17 +10,20 @@ class RemoteImporter:
         
     def find_module(self, fullname, path=None):
         if fullname in self.module_names:
-            self.path = "https://github.com/Yohan-Bismuth/hadoop-mesos/blob/master/{0}.py".format(lib)
+            self.path = "https://raw.githubusercontent.com/Yohan-Bismuth/hadoop-mesos/master/{0}.py".format(fullname)
+            print(self.path)
             return self
         return None
 
     def load_module(self, path):
+        print(self.path)
+        path=self.path
         with urllib.request.urlopen(path) as stream:
             code = stream.read()
             
-            compiled_code = compile(code, url, 'exec')
-            module = Module(parts)
+            compiled_code = compile(code, path, 'exec')
+            module = Module(path)
             eval(compiled_code, locals=module.__dict__)
             return module
         
-sys.meta_path.append(GitHubImporter('test_lib'))
+sys.meta_path.append(RemoteImporter('test_lib'))
